f57ad81b18d86c8d8c02156f52f747bf54a43d2a
Daniel <danieldelacruzprieto@gmail.com>
2020-11-13 02:03:29 -0500

    poniendo el codigo de python en el ejercicio del Hydra

--- 243B.Hydra/hydra.cpp
+++ 243B.Hydra/hydra.cpp
@@ -1,274 +1,28 @@
 #include <stdio.h>
 #include <iostream>
-#include <vector> 
+#include <vector>
 
 using namespace std ; 
-#define MAX 10^5
+#define MAX 100000
 #define TUPLE pair <int,int> 
 
 vector<int> graph [MAX];
 bool visited [MAX] ;  
 vector <TUPLE> aristas;
 vector <int> tail , head ; 
-
-
 int n,m,h,t ; 
 
-
-void solution ()
-{   
-    int i , j , u , v , a , deg_u , deg_v , count , comun ,ind_u , ind_v ; 
-    for(i=0 ; i<m ; i++)
-    {
-        TUPLE e = aristas[i];
-        u = e.first ;
-        v = e.second ; 
-        deg_u = graph[u].size() ;
-        deg_v = graph[v].size();
-
-
-        if ( (deg_u < h+1 | deg_v < t + 1) && (deg_u < t+1 | deg_v < h+1))
-            continue ; 
-
-        if (deg_u >= h+t+1) 
-        {
-            count = 0; 
-            for (j =0 ; j <= deg_v ; j++)
-            {
-                a = graph[v][j]; 
-                if (a == u)
-                {
-                    continue;
-                }
-                visited[a] = true ;
-                tail.push_back(a);
-                count = count + 1 ; 
-                if (count >= t )
-                {
-                    break ; 
-                }
-            }
-            count = 0 ; 
-            for (j=0 ; j<= deg_u ; j++)
-            {
-                a = graph[u][j]; 
-                if (a == v)
-                {
-                    continue ; 
-                }
-                visited[a] = true ; 
-                head.push_back(a);
-                count = count + 1 ; 
-                if (count >= h)
-                {
-                    break; 
-                }
-            }
-        }
-        if (deg_v >= h+t+1)
-        {
-            count = 0 ; 
-            for (j=0 ; j<deg_u ; j++)
-            {
-                a = graph[u][j];
-                if (a == v)
-                {
-                    continue ; 
-                }
-                visited[a] = true ; 
-                tail.push_back(a);
-                count = count + 1 ; 
-                if (count >= t )
-                {
-                    break; 
-                }
-            }
-            count = 0 ; 
-            for (j=0 ; j < deg_v ; j++)
-            {
-                a = graph[v][j]; 
-                if (a== u)
-                {
-                    continue;
-                }
-                visited[a] = true ; 
-                head.push_back(a);
-                count = count + 1 ;
-                if (count >= h )
-                {
-                    break; 
-                }
-            }
-        }
-
-        for (j=0 ; j<deg_u ; j++)
-        {
-            a = graph[u][j];
-            if (a == v)
-            {
-                continue ;
-            }
-            visited[a] = true ; 
-        }
-        for (j=0 ; j<deg_v ; j++)
-        {
-            a = graph[v][j];
-            if (a == u)
-            {
-                continue;
-            }
-            if (visited[a])
-            {
-                comun = comun +1 ;
-            }
-              
-        }
-
-        ind_u = deg_u - comun -1; 
-        ind_v = deg_v - comun -1; 
-
-        if (deg_u >= h+1 && deg_v >=t+1)
-        {
-            if (h - ind_u + t -ind_v > comun)
-                continue ;
-
-
-            int select_commun = 0 ; 
-            for (j=0 ; j < deg_v ; j++)
-            {
-                if (tail.size() >= t )
-                {
-                    break; 
-                }
-                a = graph[v][j];
-                if (visited[a])
-                {
-                    if (select_commun < t - ind_v )
-                    {
-                        tail.push_back(a);
-                        visited[a] = false; 
-                        select_commun = select_commun + 1 ;
-                    }
-                }
-                else
-                {
-                    tail.push_back(a);
-                }
-            } 
-            for (j=0 ; j < deg_u ; j++)
-            {
-                if (head.size() >= t)
-                {
-                    break; 
-                }
-                a = graph[u][j];
-                if (visited[a])
-                {
-                    head.push_back(a);
-                }
-            }          
-
-        }
-
-
-
-          
-    } 
-} 
-
-
-int shortsolution (int u , int v ) 
+int vertexcomun (int u , int v )
 {
-    int deg_u = graph[u].size(); 
-    int deg_v = graph[v].size();
-    int count , i , a ; 
-
-    if (deg_u < h+1 || deg_v < t+1)
-    {
-        return 0 ; 
-    }
-    if (deg_u >= h+t+1)
-    {
-        count = 0; 
-        for (i=0 ; i < deg_v ; i++)
-        {
-            a = graph[v][i]; 
-            if (a == u ) 
-            {
-                continue ; 
-            }
-            visited[a] = true ; 
-            count = count + 1 ; 
-            tail.push_back (a) ; 
-            if (count >= t )
-            {
-                break ; 
-            }
-        }
-        count =0 ; 
-        for (i=0 ; i < deg_u ; i++)
-        {
-            a = graph[u][i] ; 
-            if (visited[a] || a == v ) 
-            {
-                continue ; 
-            }
-            count = count + 1 ; 
-            head.push_back(a); 
-            if (count >= h )
-            {
-                break ; 
-            }
-        }
-        return 1 ; 
-    }
-    if (deg_v >= h+t+1)
-    {
-        count = 0; 
-        for (i=0 ; i < deg_u ; i++)
-        {
-            a = graph[u][i]; 
-            if (a == v ) 
-            {
-                continue ; 
-            }
-            visited[a] = true ; 
-            count = count + 1 ; 
-            head.push_back (a) ; 
-            if (count >= h )
-            {
-                break ; 
-            }
-        }
-        count =0 ; 
-        for (i=0 ; i < deg_v ; i++)
-        {
-            a = graph[v][i] ; 
-            if (visited[a] || a == u ) 
-            {
-                continue ; 
-            }
-            count = count + 1 ; 
-            tail.push_back(a); 
-            if (count >= t )
-            {
-                break ; 
-            }
-        }
-        return 1 ; 
-    }
     int communvertex = 0 ; 
-
-    for (i=0 ; i< deg_u ; i++)
+    int i , a ; 
+    for (i=0 ; i< graph[u].size() ; i++)
     {
         a = graph [u][i];
-        if (a == v)
-        {
-            continue ; 
-        }
+        if (a == v)continue; 
         visited[a] = true ;  
     }
-    for (i=0 ;i<deg_v ; i++)
+    for (i=0 ;i<graph[v].size() ; i++)
     {
         a = graph[v][i];
         if (visited[a])
@@ -276,29 +30,89 @@ int shortsolution (int u , int v )
             communvertex = communvertex + 1; 
         }
     }
+    return communvertex;
+} 
 
-    if ( (h-(deg_u -1 -communvertex) + t - (deg_v-1 - communvertex)) > communvertex)
-    {
-        for (i =0 ; i< deg_u ; i++)
+void restorevisitedarray (int u )
+{
+    int i , a ;
+    for (i =0 ; i< graph[u].size() ; i++)
         {
             a = graph[u][i] ; 
             visited[a] = false;
         }
-        return 0 ; 
+} 
 
+int shortsolution (int u , int v ) 
+{
+    int deg_u = graph[u].size(); 
+    int deg_v = graph[v].size();
+    int count , i , a , communvertex; 
+
+    if (deg_u < h+1 || deg_v < t+1)
+    {
+        return 0 ; 
+    }
+
+
+
+    if (deg_u >= h+t+1)
+    {
+        for (i=0 ; i < deg_v ; i++)
+        {
+            if (tail.size() == t ) break; 
+            a = graph[v][i]; 
+            if (a == u ) continue ;
+            visited[a] = true ; 
+            tail.push_back (a) ; 
+        }
+        for (i=0 ; i < deg_u ; i++)
+        {
+            if (head.size() == h )break;
+            a = graph[u][i] ; 
+            if (visited[a] || a == v ) 
+            {
+                continue ; 
+            }
+            head.push_back(a); 
+        }
+        return 1 ; 
+    }
+    if (deg_v >= h+t+1)
+    {
+        for (i=0 ; i < deg_u ; i++)
+        {
+            if (head.size() ==h)break; 
+            a = graph[u][i]; 
+            if (a == v ) continue ; 
+            visited[a] = true ; 
+            head.push_back (a) ; 
+        }
+        for (i=0 ; i < deg_v ; i++)
+        {
+            if (tail.size() == t) break ;
+            a = graph[v][i] ; 
+            if (visited[a] || a == u ) continue ; 
+            tail.push_back(a); 
+        }
+        return 1 ; 
+    }
+   
+    communvertex = vertexcomun(u,v);
+
+    if ( (h-(deg_u -1 -communvertex) + t - (deg_v-1 - communvertex)) > communvertex)
+    {
+        restorevisitedarray(u);
+        return 0 ; 
     }
 
     int countcommun = 0 ; 
 
     for (i=0 ; i<deg_v ; i++)
     {
-        
-        if (tail.size() == t )
-        {
-            break; 
-        }
+        if (tail.size() == t ) break; 
         a = graph[v][i]; 
-        if (visited[a] && countcommun < t - (deg_v -communvertex) )
+        if (visited[a] && countcommun < t - (deg_v-1 -communvertex) )
         {
             countcommun = countcommun + 1 ; 
             visited[a] = false ; 
@@ -311,61 +125,65 @@ int shortsolution (int u , int v )
     }
     for (i=0 ; i<deg_u ; i++)
     {
-        if (head.size() == h)
-        {
-            break; 
-        }
+        if (head.size() == h) break; 
         a = graph[u][i];
-        if (!visited[a])
-        {
-            continue; 
-        }
-        else 
-        {
-            head.push_back(a);
-        }
+        if (!visited[a]) continue; 
+        head.push_back(a);
     }
     return 1 ; 
 }
 
+void printsolution (int u , int v)
+{
+    cout << "YES" << endl; 
+    cout << u + 1 << " " << v + 1  <<endl;
+    int ih , it; 
+    for (ih = 0 ; ih < head.size() ; ih ++)
+    {
+        cout << head[ih] + 1 << " ";
+    } 
+    cout << endl;
+    for (it = 0; it < tail.size() ; it ++)
+    {
+        cout <<tail[it] + 1 << " "; 
+    }
+    cout << endl ;
+} 
+
 int main () {
     cin >> n >> m >> h >> t ;
-    int a , b , i , j ;  
+    int a , b , i ;  
 
     i=0 ;
     while (i++ < m  )
     {
         cin >> a >> b ; 
-        aristas.push_back(TUPLE(a,b));
+        aristas.push_back(TUPLE(a-1,b-1));
         graph[a-1].push_back(b-1);
         graph[b-1].push_back(a-1);
     }
     
     int u , v ; 
-    for (u=0 ; u< n ; u++)
+   
+    for (i=0 ; i < aristas.size(); i++)
     {
-        for ( j = 0 ; j < graph[u].size() ; j++ )
+        u = aristas[i].first ;
+        v = aristas[i].second;
+
+        if (shortsolution(u,v))
         {
-            v = graph[u][j];
-            if (shortsolution(u,v))
-            {
-                cout << "YES" << endl; 
-                cout << u + 1 << " " << v + 1  <<endl;
-                int ih , it; 
-                for (ih = 0 ; ih < head.size() ; ih ++)
-                {
-                    cout << head[ih] + 1 << " ";
-                } 
-                cout << endl;
-                for (it = 0; it < tail.size() ; it ++)
-                {
-                    cout <<tail[it] + 1 << " "; 
-                }
-                cout << endl ;
-                return 0 ; 
-            }
+            printsolution(u,v);
+            return 0 ; 
+        }
+        else if (shortsolution(v,u))
+        {
+            printsolution(v,u);
+            return 0 ;
         }
     }
-    cout << "YES" <<endl ; 
+
+    
+
+    cout << "NO" <<endl ; 
     return 0 ; 
 }
--- 243B.Hydra/hydra.py
+++ 243B.Hydra/hydra.py
@@ -1,54 +1,160 @@
+graph = []
+visited = [] 
+head = []
+tail = [] 
+n,m,h,t,i = 0,0,0,0,0
 
-class Vertex (object):    # this class is used tu represent a vertex an her neighbors
+def vertexcommun (u,v):
+    count = 0 
+    i=0
+    while i < len(graph[u]):
+        a = graph[u][i]
+        i=i+1
+        if a == v : continue 
+        visited[a] = True 
+    i=0
+    while i < len(graph[v]):
+        a = graph[v][i]
+        i= i+1
+        if visited[a]:
+            count = count +1 
+    return count 
 
-    def __init__(self,n):
-        self.name = n   # vertex 
-        self.neighbor = list() # list of vertex that are its neighbors
+def restorevisitedarray (u):
+    i=0
+    while i < len(graph[u]):
+        a = graph[u][i]
+        i = i + 1 
+        visited [a] = False
 
-    def addneighbor (self , v ):  # this is to add neighbors to the list
-        if v not in self.neighbor:  
-            self.neighbor.append(v)
-            self.neighbor.sort() 
+def solution (u,v):
+    deg_u = len (graph[u])
+    deg_v = len (graph[v])
+    
+    if deg_u < h+1 or deg_v < t+1:
+        return False 
+
+    if deg_u >= h+t+1:
+        i=0
+        while i < deg_v:
+            if len(tail) == t : break 
+            a = graph[v][i]
+            i = i + 1 
+            if a == u : continue 
+            visited[a] = True 
+            tail.append(a)
+        i=0
+        while i < deg_u:
+            if len(head) == h : break
+            a = graph[u][i]
+            i = i +1 
+            if a == v  or visited[a]: continue 
+            head.append(a)
+        return True
+
+    if deg_v > h+t+1:
+        i=0 
+        while i  < deg_u:
+            if len(head) == h :break
+            a = graph[u][i]
+            i = i + 1 
+            if a == v : continue 
+            visited[a]= True 
+            head.append(a)
+        i=0 
+        while i < deg_v:
+            if len(tail) == t : break
+            a= graph[v][i]
+            i = i+1
+            if a == u or visited[a] : continue
+            tail.append(a)
+        return True 
+        
+    communvertex = vertexcommun(u,v)
+    ind_u = deg_u -1 - communvertex
+    ind_v = deg_v -1 - communvertex
+
+    if h - ind_u + t - ind_v > communvertex:
+        restorevisitedarray(u)
+        return False 
+
+    countcommun = 0 
+    i=0
+    while i < deg_v:
+        if len(tail) == t : break 
+        a = graph[v][i]
+        i = i + 1
+        if visited[a] and countcommun < t - ind_v:
+            countcommun = countcommun + 1
+            visited[a] = False 
+            tail.append(a)
+        elif visited[a] == False  and a != u :
+            tail.append(a)
+    i=0 
+    while i < deg_u:
+        if len(head)==h : break 
+        a = graph[u][i]
+        i=i+1
+        if  visited[a] == False : continue 
+        head.append(a)  
+    return True 
 
 
-class Graph (object): # this represent the graph 
-    vertices = {}  
+def init (n):
+    i =0 ; 
+    while i < n:
+        graph.append([])
+        visited.append(False)
+        i= i+1  
 
-    def addvertex (self , vertex ):
-        if isinstance(vertex,Vertex):
-            self.vertices[vertex.name] = vertex
-            return True 
-        else:
-            return False 
-
-
-    def addedge (self ,u , v):  #this is 
-        if u in self.vertices and v in self.vertices:
-            self.vertices[u].addneighbor(v)
-            self.vertices[v].addneighbor(u)
-            return True
-        else:
-            return False 
-
-
-
-    def printGraph (self):  # this should be printed the graph 
-        for key in sorted(list(self.vertices.keys())):
-            print( str(key) + ' ' + str( self.vertices[key].neighbor ) )
-           
+def printsolution (u,v):
+    print("YES")
+    print (str(u+1) + " " + str(v+1))
+    for item in head:
+        print (str (item+1) , end = ' ')
+    print()
+    for item in tail:
+        print(str(item+1),end = ' ')
+    print()
 
 
 if __name__ == '__main__':
-    g = Graph() 
+    
+    line = input().split()
+    n = int(line[0])
+    m = int(line[1])
+    h = int(line[2])
+    t = int(line[3])
 
-    for i in range (1,6):
-        g.addvertex(Vertex(i))
+    edges = [] 
 
-    bordes = ['53','54','31','35','41','42','45','12','13','14','21','24']
+    init(n)
 
-    for borde in bordes:
-        g.addedge(int(borde[:1]), int (borde[1:]))
-       
-    g.printGraph()
+    i=0
+    while i < m:
+        line = input().split()
+        a , b = int (line[0]) , int (line[1])
+        edges.append((a-1,b-1))   
+        graph[a-1].append(b-1)
+        graph[b-1].append(a-1)  
+        i = i+1 
+    
+    flag = False
+    for item in edges :
+        u , v = item[0] , item [1]
+
+        if solution(u,v):
+            printsolution(u,v)
+            flag = True
+            break 
+        elif solution(v,u):
+            printsolution(v,u)
+            flag = True
+            break 
+
+    if not flag : 
+        print("NO")
+
+        
 
     
