cc3315ddeebffb94ebe11e2aea0b53845f954dc0
Daniel <danieldelacruzprieto@gmail.com>
2020-11-14 11:40:17 -0500

    haciendo algunos cambios en el informe de charlyandnito

---
+++ 1076D.EdgeDeletion/edgedeletion.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#define Tuple pair<int,int> 
+
+using namespace std ; 
+
+int main (int argc ,char ** argv ) 
+{
+    set<pair<long long , int > >   s ; 
+    int n = 10 ; 
+    vector<int> last (n,-1) ;
+    int i =0 ; 
+    for (size_t  i = 0; i  < last.size(); i++)
+    {
+        cout << last[i] << endl ;  
+    }
+     
+}
